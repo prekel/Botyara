4ce5327c05d63bbd37cd1c967310aa512ccd7bc2
Vladislav Prekel <misterptits@yandex.ru>
2018-10-08 00:42:23 +0700

    deploy condition (deploy)

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,3 +1,5 @@
+$env:need_deploy = $env:APPVEYOR_REPO_COMMIT_MESSAGE.Contains("(deploy)")
+
 $env:rev = git rev-list HEAD --count
 $env:assembly_version = "$env:APPVEYOR_BUILD_VERSION.$env:rev"
 Write-Host -Backgroundcolor DarkGreen -Foregroundcolor White "Assembly Version: $env:assembly_version"
