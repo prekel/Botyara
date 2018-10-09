e315c39d5a0f39b396bacbc2cb66b6d064591167
Vladislav Prekel <misterptits@yandex.ru>
2018-10-09 21:00:45 +0700

    create Schedule

---
+++ Botyara.Core/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Botyara.Core
+{
+	class Schedule : Dictionary<DateTime, string>
+	{
+		public long PeerId { get; private set; }
+
+		public IList<string> Targets { get; private set; }
+
+		public Schedule()
+		{
+
+		}
+	}
+}
