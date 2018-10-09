c69a7be070962b3a8f4ee84180495696052341d4
Vladislav Prekel <misterptits@yandex.ru>
2018-10-10 00:50:50 +0700

    attributes

--- Botyara.Core/Schedule.cs
+++ Botyara.Core/Schedule.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Botyara.Core
 {
+	[JsonObject]
 	class Schedule : Dictionary<DateTime, string>
 	{
+		[JsonProperty]
 		public long PeerId { get; private set; }
 
+		[JsonProperty]
 		public IList<string> Targets { get; private set; }
 
 		public Schedule()
