dfe05b39733f96838ca0d78ae79595fa0bd9efa8
Vladislav Prekel <misterptits@yandex.ru>
2018-10-10 21:21:51 +0700

    configs

---
+++ Botyara.Core/Configs/ChatConfig.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Botyara.Core.Configs
+{
+	[JsonObject]
+	public class ChatConfig
+	{
+		/// <summary>
+		/// ID беседы
+		/// </summary>
+		[JsonProperty]
+		public long PeerId { get; set; }
+		
+		/// <summary>
+		/// Номера групп
+		/// </summary>
+		[JsonProperty]
+		public IList<string> Targets { get; set; }
+		
+		/// <summary>
+		/// "Расписание на {OddEvenDayVinPod}:"
+		/// Расписание на чётный понедельник:
+		/// </summary>
+		[JsonProperty]
+		public string FirstString { get; set; }
+		
+		/// <summary>
+		/// "Группа {TargetId}"
+		/// Группа КИ18-17/1б:
+		/// </summary>
+		[JsonProperty]
+		public string SecondString { get; set; }
+		
+		/// <summary>
+		/// "{Number}) {Time} {Subject} ({Type}) {Teacher} {Place}"
+		/// 2) 10:15-11:50 Основы программирования (практика) Грузенкин Д. В. УЛК423
+		/// </summary>
+		[JsonProperty]
+		public string LessonString { get; set; }
+	}
+}
---
+++ Botyara.Core/Configs/Config.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Botyara.Core.Configs
+{
+	[JsonObject]
+	public class Config
+	{
+		[JsonProperty]
+		public string AccessToken { get; set; }
+		
+		[JsonProperty]
+		public IList<ChatConfig> ChatConfigs { get; set; }
+	}
+}
