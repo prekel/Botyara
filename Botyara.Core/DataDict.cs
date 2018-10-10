d5f3466cfb272ceb1e3a1f34150b86ec1da1b287
Vladislav Prekel <misterptits@yandex.ru>
2018-10-10 23:46:02 +0700

    not used methods

--- Botyara.Core/DataDict.cs
+++ Botyara.Core/DataDict.cs
@@ -12,10 +12,10 @@ namespace Botyara.Core
 		public StudyTimetable Timetable { get; private set; }
 		public Day Day { get; private set; }
 		public Week Week { get; private set; }
-		
+
 		public int CurrentTarget { get; set; }
 		public int CurrentLesson { get; set; }
-		
+
 		public DataDict(ChatConfig config, StudyTimetable timetable, Day day, Week week)
 		{
 			Config = config;
@@ -23,67 +23,9 @@ namespace Botyara.Core
 			Day = day;
 			Week = week;
 		}
-		
-		public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
-		{
-			throw new NotImplementedException();
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
-
-		public void Add(KeyValuePair<string, object> item)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Clear()
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool Contains(KeyValuePair<string, object> item)
-		{
-			throw new NotImplementedException();
-		}
-
-		[Obsolete]
-		public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
-		{
-			throw new NotImplementedException();
-		}
-
-		[Obsolete]
-		public bool Remove(KeyValuePair<string, object> item)
-		{
-			throw new NotImplementedException();
-		}
 
 		public int Count { get; }
-
 		public bool IsReadOnly { get; } = true;
-
-		public void Add(string key, object value)
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool ContainsKey(string key)
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool Remove(string key)
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool TryGetValue(string key, out object value)
-		{
-			throw new NotImplementedException();
-		}
 
 		public object this[string key]
 		{
@@ -102,7 +44,77 @@ namespace Botyara.Core
 			set => throw new NotImplementedException();
 		}
 
-		public ICollection<string> Keys { get; }
-		public ICollection<object> Values { get; }
+		#region NotUsed
+
+		[Obsolete]
+		public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		[Obsolete]
+		public void Add(KeyValuePair<string, object> item)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public bool Contains(KeyValuePair<string, object> item)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public bool Remove(KeyValuePair<string, object> item)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public void Add(string key, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public bool ContainsKey(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public bool Remove(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete]
+		public bool TryGetValue(string key, out object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Obsolete] public ICollection<string> Keys { get; }
+		[Obsolete] public ICollection<object> Values { get; }
+
+		#endregion
 	}
 }
