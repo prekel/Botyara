56f1dee3b73da6ce3f0040bb584807134c991832
Vladislav Prekel <misterptits@yandex.ru>
2018-10-11 02:14:14 +0700

    new answerer

--- Botyara.Console/Program.cs
+++ Botyara.Console/Program.cs
@@ -1,34 +1,82 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using Botyara.Console;
 using System.Linq;
 using System.Threading;
+using static System.Console;
 using Botyara.SfuApi;
 using Botyara.Core;
-using static System.Console;
+using Botyara.Core.Configs;
+using Newtonsoft.Json;
+using VkNet.Enums;
 
 namespace Botyara.Console
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static Config Config { get; set; }
+
+		public static void Main(string[] args)
 		{
-//			var c = new TimetableBuilder("КИ18-17/1б");
-//			var t = c.Get();
-//			var q1 = (from i in t.Timetable where i.Day == 5 && i.Week == 1 select i.Subject).ToList();
-//		
-			var token  = new StreamReader("token.txt").ReadLine(); 
-			var auth = new Authorizer(token);
-//			var t1 = new Test(auth.Api);
-//			t1.Run();
-			
-			var lp = new LongPoller(auth.Api, 172122256);
-			//lp.ResponseReceived += LpOnResponseReceived;
+			if (String.Join("; ", Directory.GetFiles(".")).Contains("config.json"))
+			{
+				Config = JsonConvert.DeserializeObject<Config>(new StreamReader("config.json").ReadToEnd());
+			}
+			else
+			{
+				var token = "access_token";
+				var groupid = 1234ul;
+				var target = "КИ18-17/1б";
+
+				bool Dialog()
+				{
+					WriteLine("Ввести AccessToken, GroupId и Target? [Y/n]");
+					var ans1 = ReadLine();
+					if (!string.IsNullOrEmpty(ans1) && ans1[0].ToString().ToLower() != "y") return false;
+					Write("AccessToken: ");
+					token = ReadLine();
+					Write("GroupId: ");
+					groupid = UInt64.Parse(ReadLine());
+					Write("Target: ");
+					target = ReadLine();
+					return true;
+				}
+
+				var ret = Dialog();
+
+				var config1 = new ChatConfig
+				{
+					FirstString = "Расписание на {OddEvenDayVinPod}:",
+					SecondString = "Группа {TargetsList}:",
+					LessonString = "{NumberInTimetable}) {Time} {Subject} ({Type}) {Teacher} {Place}",
+					NoLessons = "Нет пар",
+					PeerId = 0,
+					Targets = new List<string>(new[] {target})
+				};
+				Config = new Config
+				{
+					AccessToken = token,
+					GroupId = groupid,
+					ChatConfigs = new List<ChatConfig>(new[] {config1})
+				};
+
+				var json = JsonConvert.SerializeObject(Config, Formatting.Indented);
+				using (var w = new StreamWriter("config.json"))
+				{
+					w.WriteLine(json);
+				}
+
+				if (!ret) return;
+			}
+
+			var auth = new Authorizer(Config.AccessToken);
+
+			var lp = new LongPoller(auth.Api, Config.GroupId);
 			lp.Start();
 			lp.Run();
-			
-			var t1 = new Test(auth.Api, lp, "КИ18-17/1б");
-			//t1.Run();
+
+			var t1 = new Answerer(auth.Api, lp, Config);
 
 			while (true)
 			{
---
+++ Botyara.Core/Answerer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Botyara.Core.Configs;
+using VkNet;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams;
+using Newtonsoft.Json;
+using Botyara.SfuApi;
+
+namespace Botyara.Core
+{
+	public class Answerer
+	{
+		public VkApi Api { get; private set; }
+		public LongPoller LongPoller { get; private set; }
+		public Config Config { get; private set; }
+
+		public Answerer(VkApi api, LongPoller lp, Config config)
+		{
+			Api = api;
+			LongPoller = lp;
+			lp.ResponseReceived += LpOnResponseReceived;
+			Config = config;
+		}
+
+		private void LpOnResponseReceived(object sender, EventArgs e)
+		{
+			var lpe = (LongPollResponseEventArgs) e;
+			var resp = lpe.RawResponse;
+			var resp1 = lpe.Response;
+
+			Console.WriteLine(resp.RawJson);
+
+			if (resp1.Failed != null)
+			{
+				LongPoller.Start();
+				return;
+			}
+
+			if (resp1.Updates.Count == 0)
+			{
+				return;
+			}
+
+			try
+			{
+				var msg = resp1.Updates[0].Object;
+				var msgtext = msg.Text;
+
+				var spl = msgtext.Split();
+				var a = 0;
+				var b = 0;
+				if (spl.Length != 2) return;
+				a = Int32.Parse(spl[0]);
+				b = Int32.Parse(spl[1]);
+
+				var chatconf = (from i in Config.ChatConfigs where i.PeerId == msg.PeerId select i).First();
+				var compiler = new Compiler(chatconf);
+				var ans = compiler.Compile((Day) a, (Week) b);
+
+				var typ = resp1.Updates[0].Type;
+				if (msgtext != "" && typ == "message_new")
+				{
+					Api.Messages.Send(new MessagesSendParams
+					{
+						PeerId = msg.PeerId,
+						Message = ans
+					});
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+		
+		[Obsolete]
+		private void LpOnResponseReceived1(object sender, EventArgs e)
+		{
+			var lpe = (LongPollResponseEventArgs) e;
+			var resp = lpe.RawResponse;
+			var resp1 = lpe.Response;
+
+			Console.WriteLine(resp.RawJson);
+
+			if (resp1.Failed != null)
+			{
+				LongPoller.Start();
+				return;
+			}
+
+			if (resp1.Updates.Count == 0)
+			{
+				return;
+			}
+
+			try
+			{
+				var msg = resp1.Updates[0].Object.Text;
+
+				var spl = msg.Split();
+				var a = 0;
+				var b = 0;
+				if (spl.Length != 2) return;
+				a = Int32.Parse(spl[0]);
+				b = Int32.Parse(spl[1]);
+
+				var c = new TimetableBuilder("КИ18-17/1б");
+				var t = c.Get();
+				msg = String.Join(", ", from i in t.Timetable where (int)i.Day == a && (int)i.Week == b select i.Subject);
+
+				var typ = resp1.Updates[0].Type;
+				if (msg != "" && typ == "message_new")
+				{
+					Api.Messages.Send(new MessagesSendParams
+					{
+						PeerId = resp1.Updates[0].Object.PeerId,
+						Message = msg
+					});
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+	}
+}
