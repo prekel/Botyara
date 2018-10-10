13e46eee06988af0d0c3bd79c12ab83fd1783ff0
Vladislav Prekel <misterptits@yandex.ru>
2018-10-11 02:17:07 +0700

    not work test

--- Botyara.Core.Tests/CompilerTests.cs
+++ Botyara.Core.Tests/CompilerTests.cs
@@ -10,13 +10,16 @@ namespace Botyara.Core.Tests
 	[TestFixture]
 	public class CompilerTests
 	{
-		private ChatConfig Config { get; set; }
-		private Compiler Compiler { get; set; }
+		private ChatConfig Config1 { get; set; }
+		private Compiler Compiler1 { get; set; }
+
+		private ChatConfig Config2 { get; set; }
+		private Compiler Compiler2 { get; set; }
 
 		[SetUp]
 		public void Setup()
 		{
-			Config = new ChatConfig
+			Config1 = new ChatConfig
 			{
 				FirstString = "Расписание на {OddEvenDayVinPod}:",
 				SecondString = "Группа {TargetsList}:",
@@ -25,13 +28,24 @@ namespace Botyara.Core.Tests
 				PeerId = 0,
 				Targets = new List<string>(new[] {"КИ18-17/1б"})
 			};
-			Compiler = new Compiler(Config);
+			Compiler1 = new Compiler(Config1);
+
+			Config2 = new ChatConfig
+			{
+				FirstString = "Расписание на {OddEvenDayVinPod}:",
+				SecondString = "Группа {TargetsList}:",
+				LessonString = "{NumberInTimetable}) {Time} {Subject} ({Type}) {Teacher} {Place}",
+				NoLessons = "Нет пар",
+				PeerId = 0,
+				Targets = new List<string>(new[] {"КИ18-17/1б"})
+			};
+			Compiler2 = new Compiler(Config1);
 		}
 
 		[Test]
-		public void Test12()
+		public void Test1_12()
 		{
-			var res = Compiler.Compile(Day.Monday, Week.Even);
+			var res = Compiler1.Compile(Day.Monday, Week.Even);
 			var exc = @"Расписание на чётный понедельник:
 Группа КИ18-17/1б:
 1) 08:30-10:05 Введение в инженерную деятельность (практика) Пересунько П. В. пр./УЛК410, УЛК306
@@ -41,12 +55,28 @@ namespace Botyara.Core.Tests
 		}
 
 		[Test]
-		public void Test32()
+		public void Test1_32()
 		{
-			var res = Compiler.Compile(Day.Wednesday, Week.Even);
+			var res = Compiler1.Compile(Day.Wednesday, Week.Even);
 			var exc = @"Расписание на чётную среду:
 Группа КИ18-17/1б:
 Нет пар
+";
+			Assert.AreEqual(exc, res);
+		}
+
+		[Test]
+		public void Test2_12()
+		{
+			var res = Compiler2.Compile(Day.Monday, Week.Even);
+			var exc = @"Расписание на чётный понедельник:
+Группа КИ18-17/1б:
+1) 08:30-10:05 Введение в инженерную деятельность (практика) Пересунько П. В. пр./УЛК410, УЛК306
+2) 10:15-11:50 Основы программирования (практика) Грузенкин Д. В. УЛК423
+Группа КИ18-17/2б:
+1) 08:30-10:05 Математический анализ (практика) Белько Е. С. УЛК512
+2) 10:15-11:50 Введение в инженерную деятельность (практика) Пересунько П. В. пр./УЛК410, УЛК306
+3) 12:00-13:35 Основы программирования (практика) Черниговский А. С. УЛК423
 ";
 			Assert.AreEqual(exc, res);
 		}
