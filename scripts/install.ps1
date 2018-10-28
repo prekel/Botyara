361ab5310e3d71ec92fcabd7de11659f8483ba5f
Vladislav Prekel <misterptits@yandex.ru>
2018-10-28 22:15:31 +0700

    update version

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,5 +1,8 @@
 $env:rev = git rev-list HEAD --count
 $env:assembly_version = "$env:APPVEYOR_BUILD_VERSION.$env:rev"
 $env:APPVEYOR_BUILD_VERSION = $env:assembly_version
+
+Update-AppveyorBuild -Version "$env:assembly_version"
+
 Write-Host -Backgroundcolor DarkGreen -Foregroundcolor White "Assembly Version: $env:assembly_version"
 Write-Host -Backgroundcolor DarkGreen -Foregroundcolor White "Assembly Version: $env:APPVEYOR_BUILD_VERSION"
