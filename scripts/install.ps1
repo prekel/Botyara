caf0af4010fe245efc3fd5327fbc73f6f8344c8c
Vladislav Prekel <misterptits@yandex.ru>
2018-10-28 22:46:56 +0700

    (deploy)

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -5,4 +5,7 @@ If ($env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2017")
 	Update-AppveyorBuild -Version "$env:assembly_version"
 }
 
+$env:need_deploy = $env:APPVEYOR_REPO_COMMIT_MESSAGE.Contains("(deploy)")
+$need_deploy = $env:APPVEYOR_REPO_COMMIT_MESSAGE.Contains("(deploy)")
+
 Write-Host -Backgroundcolor DarkGreen -Foregroundcolor White "Assembly Version: $env:APPVEYOR_BUILD_VERSION"
