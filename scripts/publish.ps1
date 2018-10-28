38718e9841db17d2a0d8a022d5096e02af996207
Vladislav Prekel <misterptits@yandex.ru>
2018-10-28 23:49:33 +0700

    fix

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,8 +1,1 @@
-If ($env:APPVEYOR_REPO_TAG)
-{
-	dotnet publish /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" /p:Version="$env:APPVEYOR_REPO_TAG_NAME"
-}
-Else 
-{
-	dotnet publish /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
-}
+dotnet publish /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
