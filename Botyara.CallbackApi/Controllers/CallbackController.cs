cf2a1ffa8389b0f0c2d438f3ef2c81bd4d4cf408
Vladislav Prekel <misterptits@yandex.ru>
2019-07-21 22:43:15 +0700

    netcoreapp2.2

---
+++ Botyara.CallbackApi/Controllers/CallbackController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using VkNet.Model;
+using VkNet.Utils;
+
+namespace Botyara.CallbackApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        public CallbackController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("/")]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Тип события
+            switch (updates.Type)
+            {
+                // Ключ-подтверждение
+                case "confirmation":
+                {
+                    return Ok(_configuration["Config:Confirmation"]);
+                }
+
+                // Новое сообщение
+                case "message_new":
+                {
+                    // Десериализация
+                    var msg = Message.FromJson(new VkResponse(updates.Object));
+                    break;
+                }
+            }
+
+            return Ok("ok");
+        }
+    }
+}
